9f920f17ba686cc1624c5f10992d385e4e9711da
MArcelo Rodrigues <ruysmarcelo@gmail.com>
2023-05-05 00:05:37 -0300

    Solução de erros View Produto

--- ControleEstoque/ControleEstoque/Program.cs
+++ ControleEstoque/ControleEstoque/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 string mySqlConnection =
-              builder.Configuration.GetConnectionString("EtecConnection");
+              builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContextPool<AppDbContext>(options =>
                 options.UseMySql(mySqlConnection,
